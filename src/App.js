248272af7540d2f3965e063288525b3ca0ed0220
andrei <36193348+AndreiCosa@users.noreply.github.com>
2022-07-29 22:36:30 +0300

    deployment of the header section

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 //import components
 
 import Banner from './components/Banner';
+import Header from './components/Header';
 
 const App = () => {
   return ( 
     <div>
+      <Header />
       <Banner />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+//import components
+import Logo from '../assets/img/logo.svg';
+import Nav from './Nav';
+
+//import icons
+import {HiMenu} from 'react-icons/hi';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-6'>
+    <div className="container mx-auto">
+        <div>
+          {/* logo */}
+          <a href="/" >
+            <img src={Logo} alt=''/>
+          </a>
+          {/* nav */}
+          <Nav />
+          {/* nav bar mobile */}
+          <HiMenu className='lg:hidden text-3x1 text-white cursor-pointer'/>
+        </div>
+    </div>
+  </header>;
 };
 
 export default Header;
