3123373c1456b115edf9dde3acddb81f49187931
andrei <36193348+AndreiCosa@users.noreply.github.com>
2022-07-31 11:15:38 +0300

    Adding Testimonials Section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NavMobile from './components/NavMobile';
 import Experience from './components/Experience';
 import Video from './components/Video';
 import Headsets from './components/Headsets';
+import Testimonial from './components/Testimonial';
 
 const App = () => {
   const [navMobile, setNavMobile] = useState(false);
@@ -22,6 +23,7 @@ const App = () => {
       <Experience />
       <Video />
       <Headsets />
+      <Testimonial />
     </div>
   );
 };
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+// import components
+import TestimonialSlider from './TestimonialSlider';
+
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return ( 
+    <secction className='mb-64 lg:mb-48'>
+      <div className='container mx-auto'>
+        <div className='bg-purple-400/10 max-h-[400pxp] p-12 rounded-lg'>
+          <div className='text-center'>
+            <h2 className='text-3xl font-bold mb-9'>What our clients say</h2>
+            <p className='max-w-2xl mx-auto mb-12 lg:mb-24'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+          </div>
+          {/* slider */}
+          <div>
+            <TestimonialSlider />
+          </div>
+        </div>
+      </div>
+    </secction>
+  );
 };
 
 export default Testimonial;
